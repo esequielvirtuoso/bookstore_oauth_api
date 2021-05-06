e992a1cc241109a9dc683da6cbaa362da30e77de
Esequiel Virtuoso <esequiel.virtuoso@neoway.com.br>
2021-05-06 15:20:34 -0300

    Changing cassandra session creation.

--- src/app/application.go
+++ src/app/application.go
@@ -5,7 +5,6 @@ import (
 	"github.com/esequielvirtuoso/bookstore_oauth_api/src/infrastructure/clients/cassandra"
 	"github.com/esequielvirtuoso/bookstore_oauth_api/src/infrastructure/http"
 	"github.com/esequielvirtuoso/bookstore_oauth_api/src/infrastructure/repository/db"
-	"github.com/esequielvirtuoso/bookstore_oauth_api/src/pkg/logger"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,12 +16,7 @@ func StartApplication() {
 	/*
 	* DB
 	*/
-	session, dbErr := cassandra.GetSession()
-	if dbErr != nil {
-		logger.Error("error when trying to get a new db session", dbErr)
-		panic(dbErr)
-	}
-	session.Close()
+	cassandra.GetSession()
 
 	/*
 	 * Storages
